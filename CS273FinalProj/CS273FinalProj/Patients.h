d771d77bbb9da9126adaa1ae5ae66201f4cd15ba
Oscar E. Duran <oduran18@my.whitworth.edu>
2016-12-11 09:41:09 -0800

    These are Oscar's changes

--- CS273FinalProj/CS273FinalProj/Patients.h
+++ CS273FinalProj/CS273FinalProj/Patients.h
@@ -1,8 +1,19 @@
 #ifndef PATIENTS_H_
 #define	PATIENTS_H_
 
-class Patients{
 
+
+class Patients
+{
+private:
+	string name;
+	int numOfVisits;
+	vector<int>SeverityLevel;
+public:
+	void incrementVisits;
+	int waitTime;
+	int arrivaTime;
+	int discargeTime;
 };
 
 #endif
